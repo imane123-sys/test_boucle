4761e41ec60dd2c80aa1c2eafab940f0e77c08b6
imane_ramadane <ramadaneimane8@gmail.com>
2025-09-24 15:31:13 +0100

    test des boucles

---
+++ test_boucle.js
@@ -0,0 +1,114 @@
+
+// chalenge 1:
+// let nombre_souhaite = Number(prompt("entrer un nombre que vous voulez !"));
+// for(let i=1 ;i <=10 ;i++){
+
+//     console.log(`${nombre_souhaite} x ${i} = ${nombre_souhaite * i}`)
+
+
+
+
+// }
+
+//chalenge 2
+
+// let nombre_premier = Number(prompt("entrer un nombre que vous voulez !"));
+// if (nombre_premier <= 1) {
+//   console.log("le nombre premier commance a 1");
+  
+// } else {
+//   for (let i = 2; i <= Math.sqrt(nombre_premier); i++) {
+//     if (nombre_premier % i === 0) {
+//         console.log("ce nombre est premier");
+        
+      
+//       break; 
+//     }
+//   }
+// }
+
+
+// chalenge 3
+
+
+// for(i= 10 ;i<100 ;i++){
+
+//     Math.max
+
+
+
+// }
+
+
+
+//chalenge 4
+
+// let nombre = parseInt(prompt('enter un  nombre entier'));
+// let nombreinverse =nombre.toString().split("").reverse().join("");
+
+// console.log(nombreinverse);
+
+// .split("").reverse().join("");
+// console.log(nombre);
+
+
+//chalenge 5
+
+
+// let n = parseInt(prompt("Entrez un entier n :"));
+
+// for (let i = 2; i <= n; i++) {
+//     let estPremier = true;
+
+//     for (let j = 2; j < i; j++) {
+//         if (i % j === 0) {
+//             estPremier = false;
+//             break;
+//         }
+//     }
+// }
+
+//challenge 6
+
+// let base = parseInt(prompt("Entrez la base (un nombre > 0) :"));
+// let exposant = parseInt(prompt("Entrez l'exposant (un entier ≥ 0) :"));
+
+// let resultat = 1;
+// let compteur = 0;
+
+// while (compteur < exposant) {
+//     resultat = resultat * base;
+//     compteur++;
+// }
+
+// console.log(base + "^" + exposant + " = " + resultat);
+
+
+// chalenge 7
+let somme_dix_nombres = Number(prompt("enter les 10 nombres"));
+if (somme_dix_nombres <1){
+    
+}
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
